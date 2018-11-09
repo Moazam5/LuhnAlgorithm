1463d5af2e303b7088b352da1d8c2937a2a18227
Mir Moazam Abass <m.moazam29@gmail.com>
2018-11-09 10:24:31 -0500

    Initial Commit

---
+++ luhnAlgo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  luhnAlgo
+//
+//  Created by Mir Moazam Abass on 11/9/18.
+//  Copyright © 2018 mozz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
