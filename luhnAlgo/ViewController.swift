2c787ff3e3090c5c24ccff631e25af85a7c3e40c
Mir Moazam Abass <m.moazam29@gmail.com>
2018-11-13 16:45:33 -0500

    Luhn Algorithm implemented for checking the user entered id. Here is id is supposed to be 4 a four digit id but it can be easily changed to accept any character length.

--- luhnAlgo/ViewController.swift
+++ luhnAlgo/ViewController.swift
@@ -10,11 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var invalidIDLabel: UILabel!
+    
+    var inputString = ""
+    var inputNumber = Int()
+    var array : [Character] = []
+    var intArray : [Int] = []
+    var isTrue = false
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        invalidIDLabel.isHidden = true
     }
 
+    @IBAction func send(_ sender: Any)
+    {
+       
+        if checkLuhn() == true
+        {
+     performSegue(withIdentifier: "change", sender: self)
+        }
+      else
+        {
+           print("please print valid id")
+            invalidIDLabel.isHidden = false
+        }
+    }
+    
+    
+  
+    
+    func checkLuhn() -> Bool
+    {
+        // Variables
+        var finalSum = 0
+        var sumOne : Int = 0
+        var sumTwo = 0
+        inputString = textField.text!
+
+        // Makes an array from the input in the text field
+        let digits = textField.text!.compactMap{Int(String($0))}
+        intArray = digits
+        
+        if textField.text?.count == 4
+        {
+            // Double every sencond number from the right and add them
+            for i in stride(from: intArray.count - 2, through: 0, by: -2)
+            {
+                var newNum : Int = intArray[i] * 2
+                print(newNum)
+                
+                if newNum  > 9
+                {
+                    let ones =  newNum % 10
+                    newNum = 1 + ones
+                }
+                
+                sumOne += newNum
+            }
+            // Add every alternate number
+            for i in stride(from: intArray.count - 1, through: 0, by: -2)
+            {
+                let newNum : Int = intArray[i]
+                print(newNum)
+                sumTwo += newNum
+                print(sumTwo)
+            }
+            
+            // Add the sums and take mod 10
+            finalSum = sumOne + sumTwo
+            isTrue = (finalSum % 10) == 0
+            print(isTrue)
+        }
+        
+        else
+        {
+            print("Please enter a valid 4 digit id")
+        }
+        return isTrue
+    }
 
 }
 
